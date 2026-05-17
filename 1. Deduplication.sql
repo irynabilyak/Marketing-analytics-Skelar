4e0e797170fd49a836bb97e8d768757dde533f3f
Iryna Bilyak <161013564+irynabilyak@users.noreply.github.com>
2026-05-18 01:08:01 +0300

    Update 1. Deduplication.sql

--- 1. Deduplication.sql
+++ 1. Deduplication.sql
@@ -1,11 +1,14 @@
--- Використовуємо віконну функцію ROW_NUMBER() — вона нумерує рядки всередині групи.
--- Групуємо по оголошенню і дню, сортуємо від найновішого
-SELECT
-  source,
-  COUNT(*) AS total_rows,
-  MIN(date) AS first_date,
-  MAX(date) AS last_date,
-  COUNT(DISTINCT ad_id) AS unique_ads
-FROM `marketing-analytics-skelar.marketing_raw.marketing_ads_raw`
-GROUP BY source
-ORDER BY source;
+Використовуємо віконну функцію ROW_NUMBER() — вона нумерує рядки всередині групи.
+Групуємо по оголошенню і дню, сортуємо від найновішого
+WITH deduped AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY ad_id, date
+            ORDER BY timestamp DESC
+        ) AS rn
+    FROM `marketing-analytics-skelar.marketing_raw.marketing_ads_raw`
+)
+SELECT *
+FROM deduped
+WHERE rn = 1
