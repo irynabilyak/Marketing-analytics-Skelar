c408305c9860635dadafeb74dfca0190d474c5e6
Iryna Bilyak <161013564+irynabilyak@users.noreply.github.com>
2026-05-26 23:31:34 +0300

    Refactor SQL for monthly CAC calculation

--- 1.4 CAC per month .sql
+++ 1.4 CAC per month .sql
@@ -8,22 +8,51 @@ WITH deduped AS (
     FROM `marketing-analytics-skelar.marketing_raw.marketing_ads_raw`
 ),
 
-daily AS (
+daily_last AS (
+    SELECT
+        source,
+        ad_id,
+        date,
+        spend,
+        registrations
+    FROM deduped
+    WHERE rn = 1
+),
+
+daily_delta AS (
+    SELECT
+        source,
+        ad_id,
+        date,
+        spend - LAG(spend) OVER (
+            PARTITION BY ad_id
+            ORDER BY date
+        ) AS daily_spend,
+
+        registrations - LAG(registrations) OVER (
+            PARTITION BY ad_id
+            ORDER BY date
+        ) AS daily_registrations
+
+    FROM daily_last
+),
+
+monthly AS (
     SELECT
         source,
         DATE_TRUNC(date, MONTH) AS month,
-        SUM(spend)              AS daily_spend,
-        SUM(registrations)      AS daily_registrations
-    FROM deduped
-    WHERE rn = 1
+        SUM(daily_spend)         AS monthly_spend,
+        SUM(daily_registrations) AS monthly_registrations
+    FROM daily_delta
+    WHERE daily_spend IS NOT NULL
     GROUP BY source, month
 )
 
 SELECT
     month,
     source,
-    ROUND(SUM(daily_spend), 2)                               AS total_spend,
-    ROUND(SUM(daily_spend) / SUM(daily_registrations), 2)   AS cac
-FROM daily
-GROUP BY month, source
+    ROUND(monthly_spend, 2)                                    AS total_spend,
+    monthly_registrations                                      AS total_registrations,
+    ROUND(monthly_spend / monthly_registrations, 2)           AS cac
+FROM monthly
 ORDER BY month, source
